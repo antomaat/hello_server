4964a2a4776d4df9d8ddd0de4c36079a0fac733b
anto <antoalasepp@gmail.com>
2023-12-27 21:29:32 +0200

    update server

--- src/server.c
+++ src/server.c
@@ -35,19 +35,20 @@ int main(int argc, char *argv[]) {
     listen(sock, 5);
     printf("listen to sock %d\n", sock);
 
-    struct sockaddr_in incomingSock;
-    memset(&incomingSock, 0, sizeof(incomingSock));
-    int addrSize = sizeof(incomingSock);
-    int newFd = accept(sock, &incomingSock, &addrSize);
-    printf("new connection accepted\n");
+    while (1) {
+        struct sockaddr_in incomingSock;
+        memset(&incomingSock, 0, sizeof(incomingSock));
+        int addrSize = sizeof(incomingSock);
+        int newFd = accept(sock, &incomingSock, &addrSize);
+        printf("new connection accepted\n");
 
-    char results[750];
-    recv(newFd, &results, 750, 0);
+        char results[750];
+        recv(newFd, &results, 750, 0);
 
-    printf("result %s \n", results);
+        printf("result %s \n", results);
 
-    send(newFd, "result sent", 11, 0);
-    close(newFd);
-
+        send(newFd, "result sent", 11, 0);
+        close(newFd);
+    }
 }
 
